e0da5ac0c0aea8e2975c35430079e735a8500612
Andrey Afanaskin <a.afanaskiy@artjoker.team>
2020-06-18 11:36:09 +0300

    merge

--- src/constants/componentName.ts
+++ src/constants/componentName.ts
@@ -1,8 +1,8 @@
 export const componentName = {
   // SET NEW COMPONENT NAME
-    HeaderBackButton1: 'navigation.HeaderBackButton1.component',
-    HeaderBackButton: 'navigation.HeaderBackButton.component',
-    HeaderTitle: 'navigation.HeaderTitle.component',
+  HeaderBackButton1: 'navigation.HeaderBackButton1.component',
+  HeaderBackButton: 'navigation.HeaderBackButton.component',
+  HeaderTitle: 'navigation.HeaderTitle.component',
   Profile: 'navigation.Profile.component',
   Information: 'navigation.Information.component',
   Modal: 'navigation.Modal.component',
